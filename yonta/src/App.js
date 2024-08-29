9805b17177be2040a47fe78fd6c841391a4b272a
jazooanonymous <jahangir.ameen@gmail.com>
2024-08-29 22:08:59 +0500

    updaye app

--- yonta/src/App.js
+++ yonta/src/App.js
@@ -1,9 +1,14 @@
 
+
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Toast from './components/Notifications/Toast';
 import Dashboard from './screens/Dashboard';
 import Users from './screens/users';
+import ActivityAdd from './screens/Activity/activityAdd';
+import PodCasts from './screens/poscasts';
+import DietPlans from './screens/dietPlans';
+import AddUser from './screens/addUser';
 
 function App() {  
   return (
@@ -13,10 +18,16 @@ function App() {
     
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Users/>} />
-
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/patients" element={<Users/>} />
+        <Route path="/activity" element={<ActivityAdd/>} />
+        <Route path="/podcasts" element={<PodCasts/>}/>
+        <Route path="/dietplans" element={<DietPlans/>}/>
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/addUser" element={<AddUser/>}/>
         </Routes>
-      </BrowserRouter>
+
+    </BrowserRouter>
     </>
   );
 }
